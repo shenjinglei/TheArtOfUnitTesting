8f4d3aaece3a6b1be25ee7051b8de99ecc6c4d86
shenjinglei <shenjinglei@kyee.com.cn>
2024-07-27 16:47:54 +0800

    Listing 5.2 Asserting against a handwritten fake object

---
+++ Chapter5/LogAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter5
+{
+    public class LogAnalyzer
+    {
+        private ILogger _logger;
+
+        public LogAnalyzer(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public int MinNameLength { get; set; }
+
+        public void Analyze(string filename)
+        {
+            if (filename.Length < MinNameLength)
+            {
+                _logger.LogError(string.Format("Filename too short: {0}", filename));
+            }
+        }
+    }
+
+    public interface ILogger
+    {
+        void LogError(string message);
+
+    }
+}
