24684378560dfac25b595f72ac39399837e1fa17
shenjinglei <shenjinglei@kyee.com.cn>
2024-07-28 16:24:38 +0800

    Analyze_LoggerThrows_CallsWebServiceWithNSubObject

--- Chapter5/LogAnalyzer.cs
+++ Chapter5/LogAnalyzer.cs
@@ -57,6 +57,37 @@ namespace Chapter5
         }
     }
 
+    public class LogAnalyzer3
+    {
+        private ILogger _logger;
+        private IWebService _webService;
+
+
+        public LogAnalyzer3(ILogger logger, IWebService webService)
+        {
+            _logger = logger;
+            _webService = webService;
+        }
+
+        public int MinNameLength { get; set; }
+
+        public void Analyze(string filename)
+        {
+            if (filename.Length < MinNameLength)
+            {
+                try
+                {
+                    _logger.LogError(string.Format("Filename too short: {0}", filename));
+                }
+                catch (Exception e)
+                {
+                    _webService.Write(new ErrorInfo(1000, e.Message));
+
+                }
+            }
+        }
+    }
+
     public interface ILogger
     {
         void LogError(string message);
@@ -66,7 +97,53 @@ namespace Chapter5
     public interface IWebService
     {
         void Write(string message);
+        void Write(ErrorInfo message);
     }
 
-    
+    public class ErrorInfo
+    {
+        private readonly int _severity;
+        private readonly string _message;
+
+        public ErrorInfo(int severity, string message)
+        {
+            _severity = severity;
+            _message = message;
+        }
+
+        public int Severity
+        {
+            get { return _severity; }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+        }
+
+        protected bool Equals(ErrorInfo other)
+        {
+            return _severity == other._severity && string.Equals(_message, other._message);
+        }
+
+        //this is needed to make this test pass:
+        // Analyze_LoggerThrows_CallsWebServiceWithNSubObjectCompare
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ErrorInfo)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (_severity * 397) ^ _message.GetHashCode();
+            }
+        }
+    }
+
+
 }
--- Chapter5/Tests2.cs
+++ Chapter5/Tests2.cs
@@ -27,6 +27,11 @@ namespace Chapter5
             {
                 MessageToWebService = message;
             }
+
+            public void Write(ErrorInfo message)
+            {
+                throw new NotImplementedException();
+            }
         }
 
         public class FakeLogger : ILogger
@@ -57,6 +62,23 @@ namespace Chapter5
             analyzer.Analyze("abc.txt");
 
             mockWebService.Received().Write(Arg.Is<string>(s => s.Contains("fake exception")));
+        }
+
+        [Test]
+        public void Analyze_LoggerThrows_CallsWebServiceWithNSubObject()
+        {
+            var mockWebService = Substitute.For<IWebService>();
+            var stubLogger = Substitute.For<ILogger>();
+
+            stubLogger.When(logger => logger.LogError(Arg.Any<string>()))
+                .Do(info => { throw new Exception("fake exception"); });
+
+            var analyzer = new LogAnalyzer3(stubLogger, mockWebService);
+            analyzer.MinNameLength = 8;
+            analyzer.Analyze("abc.txt");
+
+            mockWebService.Received().Write(Arg.Is<ErrorInfo>(info => info.Severity == 1000 
+                && info.Message.Contains("fake exception")));
         }
     }
 }
